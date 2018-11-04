0671170a2243768ccb7c94bcee36af69d458738f
Utz Nisslmüller <a01426012@unet.univie.ac.at>
2018-11-04 21:05:05 +0100

    Initial Maven commit incl. REST dependencies

---
+++ implementation/src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    //hallo das bin ich
+
+}
+//TODO: daniels design implementieren
+//TODO: utz' design implementieren
+//TODO: javadoc/signatures
+//TODO: REST Kommunikation
