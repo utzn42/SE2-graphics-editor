1489637d20f1f080dd81e1f577c3831f0af0074c
Daniel Sigmund <a01504922@unet.univie.ac.at>
2018-11-11 15:05:13 +0100

    Added classes to Prototype 1 design to reflect a state-based approach

--- implementation/src/HelloWorld.java
+++ implementation/src/HelloWorld.java
@@ -1,11 +1,9 @@
-
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
     //hallo das bin ich
-
 }
 //TODO: daniels design implementieren
 //TODO: utz' design implementieren
