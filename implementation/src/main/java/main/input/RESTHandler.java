592e48ee46ea53269f63a28fd7668ebb55923fa2
Utz Nisslmüller <a01426012@unet.univie.ac.at>
2018-11-11 22:04:03 +0100

    Modified InParser and method names to reflect preliminary design draft.

---
+++ implementation/src/main/java/main/input/RESTHandler.java
@@ -0,0 +1,32 @@
+package main.input;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RESTHandler {
+
+    static Logger logger = LoggerFactory.getLogger(RESTHandler.class);
+
+    @RequestMapping("/addShape/")
+    public String addShape() {
+        return "Under construction...";
+    }
+
+    @RequestMapping("/editShape/")
+    public String editShape() {
+        return "Under construction...";
+    }
+
+    @RequestMapping("/moveShape/")
+    public String moveShape() {
+        return "Under construction...";
+    }
+
+    @RequestMapping("/deleteShape/")
+    public String deleteShape() {
+        return "Under construction...";
+    }
+}
