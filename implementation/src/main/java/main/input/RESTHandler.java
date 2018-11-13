a2f6f58f7d75ee5b24cc73ee2689b2a47d3af1aa
Daniel Sigmund <a01504922@unet.univie.ac.at>
2018-11-13 14:03:32 +0100

    Added a ServerResponse class that defines the structure of the server response - Added package messages that includes the ServerResponse class and will include all message types sent by the client - Removed all uses of org.json.JSONObject for client messages and replaced them with String until proper classes are defined - RESTHandler.createProject() is now fully functional - Other RESTHandler methods now return a proper response on call, but don't change anything yet

--- implementation/src/main/java/main/input/RESTHandler.java
+++ implementation/src/main/java/main/input/RESTHandler.java
@@ -3,7 +3,7 @@ package main.input;
 import canvas.Canvas;
 import canvas.Layer;
 import facilitators.Hasher;
-import org.json.JSONObject;
+import messages.ServerResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -22,7 +22,7 @@ public class RESTHandler {
     private static long seedCounter = 0;
 
     @RequestMapping(value = "/test")
-    public Canvas test() {
+    public ServerResponse test() {
         List<Layer> layers = new ArrayList<>();
         List<Shape> shapes1 = new ArrayList<>();
         List<Shape> shapes2 = new ArrayList<>();
@@ -37,37 +37,83 @@ public class RESTHandler {
         layers.add(new Layer(shapes1));
         layers.add(new Layer(shapes2));
         layers.add(new Layer(shapes3));
-        return new Canvas(layers);
+        Canvas canvas = new Canvas(layers);
+        ServerResponse response = new ServerResponse("TestID");
+        response.setCanvas(canvas);
+        return response;
     }
 
     @RequestMapping(value = "/create")
-    public JSONObject createProject() {
+    public ServerResponse createProject() {
         Hasher hash = new Hasher(++seedCounter);
-        return null;
+        String id = hash.getHash();
+        Canvas canvas = new Canvas();
+        projects.put(id, canvas);
+        ServerResponse response = new ServerResponse(id);
+        response.setCanvas(canvas);
+        return response;
     }
 
-    @RequestMapping(value = "/addShape/{projectID}", method = RequestMethod.GET)
-    public JSONObject addShape(@PathVariable String projectID, @RequestBody JSONObject add_json) {
+    @RequestMapping(value = "/addShape/{projectID}", method = RequestMethod.POST)
+    public ServerResponse addShape(@PathVariable String projectID, @RequestBody String request) {
+        ServerResponse response = new ServerResponse(projectID);
+        if (!projects.containsKey(projectID)) {
+            response.addError(new IllegalArgumentException("Project ID " + projectID + " does not exist!"));
+            return response;
+        }
+        Canvas canvas = projects.get(projectID);
 
-        System.out.println(add_json.toString());
-        return null;
+        // TODO: Add Shape
+
+        response.setCanvas(canvas);
+        return response;
     }
 
     @RequestMapping(value = "/editShape/{projectID}", method = RequestMethod.POST)
-    public JSONObject editShape(@PathVariable String projectID, @RequestBody JSONObject edit_json) {
-        return null;
+    public ServerResponse editShape(@PathVariable String projectID, @RequestBody String request) {
+        ServerResponse response = new ServerResponse(projectID);
+        if (!projects.containsKey(projectID)) {
+            response.addError(new IllegalArgumentException("Project ID " + projectID + " does not exist!"));
+            return response;
+        }
+        Canvas canvas = projects.get(projectID);
+
+        // TODO: Edit Shape
+
+        response.setCanvas(canvas);
+        return response;
     }
 
     @RequestMapping(value = "/transformShape/{projectID}", method = RequestMethod.POST)
-    public JSONObject transformShape(@PathVariable String projectID, @RequestBody JSONObject transform_json) {
-        return null;
+    public ServerResponse transformShape(@PathVariable String projectID, @RequestBody String request) {
+        ServerResponse response = new ServerResponse(projectID);
+        if (!projects.containsKey(projectID)) {
+            response.addError(new IllegalArgumentException("Project ID " + projectID + " does not exist!"));
+            return response;
+        }
+        Canvas canvas = projects.get(projectID);
+
+        // TODO: Transform Shape
+
+        response.setCanvas(canvas);
+        return response;
     }
 
     @RequestMapping(value = "/deleteShape/{projectID}")
-    public JSONObject deleteShape(@PathVariable String projectID, @RequestBody JSONObject transform_json) {
-        return null;
+    public ServerResponse deleteShape(@PathVariable String projectID, @RequestBody String request) {
+        ServerResponse response = new ServerResponse(projectID);
+        if (!projects.containsKey(projectID)) {
+            response.addError(new IllegalArgumentException("Project ID " + projectID + " does not exist!"));
+            return response;
+        }
+        Canvas canvas = projects.get(projectID);
+
+        // TODO: Delete Shape
+
+        response.setCanvas(canvas);
+        return response;
     }
 }
 
-//TODO: Utz: Generate SHA Hash from seed
+//TODO: remove org.json.JSONObject dependency from project (no longer needed)
 //TODO: Jackson annotation reference to parse JSON to and from POJO: https://www.baeldung.com/jackson-annotations
