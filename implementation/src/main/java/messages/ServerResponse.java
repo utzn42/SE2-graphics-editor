26b92bb76b2936960d2b2317948289cc2656a3ff
Utz Nisslmüller <a01426012@unet.univie.ac.at>
2018-11-15 12:03:23 +0100

    Removed Exception Array in ServerResponse class, since an Exception is returned to the client anyhow.

--- implementation/src/main/java/messages/ServerResponse.java
+++ implementation/src/main/java/messages/ServerResponse.java
@@ -7,18 +7,15 @@ import java.util.List;
 public class ServerResponse {
 
   private String projectID;
-  private List<Exception> errors;
   private Canvas canvas;
 
   public ServerResponse() {
     projectID = null;
-    errors = new ArrayList<>();
     canvas = null;
   }
 
   public ServerResponse(String projectID) {
     this.projectID = projectID;
-    errors = new ArrayList<>();
     canvas = null;
   }
 
@@ -28,22 +25,6 @@ public class ServerResponse {
 
   public void setProjectID(String projectID) {
     this.projectID = projectID;
-  }
-
-  public List<String> getErrors() {
-    List<String> errorStrings = new ArrayList<>();
-    for (Exception error : errors) {
-      errorStrings.add(error.getClass().getSimpleName() + ": " + error.getMessage());
-    }
-    return errorStrings;
-  }
-
-  public void setErrors(List<Exception> errors) {
-    this.errors = errors;
-  }
-
-  public void addError(Exception error) {
-    this.errors.add(error);
   }
 
   public Canvas getCanvas() {
