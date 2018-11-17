7acefe207dc138a369f9075ffcc010ff7d9ac910
Utz Nisslmüller <a01426012@unet.univie.ac.at>
2018-11-17 15:28:42 +0100

    Migrated RestHandler logic to ProjectService. Created new branch for experimental implementation of desired ProjectService functionality.
    # Conflicts:
    #	implementation/src/main/java/main/input/ProjectService.java
    #	implementation/src/main/java/main/input/RESTHandler.java

--- implementation/src/main/java/main/input/ProjectService.java
+++ implementation/src/main/java/main/input/ProjectService.java
@@ -14,8 +14,9 @@ import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Observable;
 import org.apache.batik.transcoder.TranscoderException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,24 +27,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import persistency.FileManager;
 import persistency.LocalFileManager;
-import persistency.Observer;
 import shapes.Shape;
 
 @Service
-public class ProjectService implements Subject {
+public class ProjectService extends Observable {
 
   //TODO: Utz -> Make seedCounter unique. Store int in file which is retrieved upon loading Projectservice.
   private static long seedCounter = 0;
   private static Logger projectServiceLogger = LoggerFactory.getLogger(ProjectService.class);
-  private Map<String, Canvas> projects;
-  private ArrayList<Observer> observers;
+  private Map<String, Canvas> projects = new HashMap<>();
 
   public ProjectService() {
-    observers = new ArrayList<>();
     FileManager<Canvas> fileManager = new LocalFileManager<Canvas>("./projects");
-    observers.add(fileManager);
+    this.addObserver(fileManager);
     projects = fileManager.getStoredObjects();
-    notifyObservers();
+    testUpdate();
   }
 
   public String createID() {
@@ -52,11 +50,8 @@ public class ProjectService implements Subject {
 
   }
 
-  public Canvas createCanvas(String projectID) {
-    Canvas blankCanvas = new Canvas();
-    projects.put(projectID, blankCanvas);
-    notifyObservers();
-    return blankCanvas;
+  public Canvas createCanvas() {
+    return new Canvas();
   }
 
   public Canvas addLayer(String projectID) {
@@ -68,14 +63,11 @@ public class ProjectService implements Subject {
     projectServiceLogger.info("addLayer - (empty object)");
 
     canvas.getLayers().add(new Layer());
-    projects.get(projectID).getLayers().add(new Layer());
-    notifyObservers();
 
     return canvas;
   }
 
-  public Canvas addShape(String projectID, int layerIndex, String shapeClass)
-      throws Exception {
+  public Canvas addShape(String projectID, int layerIndex, String shapeClass) {
     if (!projects.containsKey(projectID)) {
       throw new IllegalArgumentException("Project ID " + projectID + " does not exist!");
     }
@@ -91,96 +83,94 @@ public class ProjectService implements Subject {
       projectServiceLogger.info("         - HTML: " + newShape.getHTML());
     } catch (ClassNotFoundException e) {
       projectServiceLogger.error("         - HTML: Failed to get Class");
-      throw new ClassNotFoundException("Class " + shapeClass + " does not exist!");
     } catch (IllegalAccessException e) {
       projectServiceLogger.error("         - HTML: Could not access Class");
-      throw new IllegalAccessException("layerIndex: " + layerIndex + ". No such layer found!");
     } catch (InstantiationException e) {
       projectServiceLogger.error("         - HTML: Could not instantiate Object for Class");
-      throw new InstantiationException("Could not instantiate object of class " + shapeClass + ".");
     }
 
-    notifyObservers();
     return canvas;
   }
 
   public Canvas editCanvas(String projectID, double width, double height) {
-    if (width < 0 || height < 0) {
-      throw new IllegalArgumentException("Height and width must both be positive!");
-    }
     if (!projects.containsKey(projectID)) {
-      throw new IndexOutOfBoundsException("Project ID " + projectID + " does not exist!");
+      throw new IllegalArgumentException("Project ID " + projectID + " does not exist!");
     }
+
+    Canvas canvas = projects.get(projectID);
 
     projectServiceLogger.info("editCanvas - Width: " + width);
     projectServiceLogger.info("           - Height: " + height);
 
-    notifyObservers();
-    return projects.get(projectID);
+    canvas.setWidth(width);
+    canvas.setHeight(height);
 
+    return canvas;
   }
 
   public Canvas editLayer(String projectID, int layerIndex, boolean isVisible) {
     if (projects.containsKey(projectID)) {
-      throw new IndexOutOfBoundsException("Project ID " + projectID + " does not exist!");
+      throw new IllegalArgumentException("Project ID " + projectID + " does not exist!");
     }
+    Canvas canvas = projects.get(projectID);
 
     projectServiceLogger.info("editLayer - Layer Index: " + layerIndex);
     projectServiceLogger.info("          - Visible: " + isVisible);
-    projects.get(projectID).getLayers().get(layerIndex).setVisible(isVisible);
+    canvas.getLayers().get(layerIndex).setVisible(isVisible);
 
-    notifyObservers();
-    return projects.get(projectID);
+    return canvas;
   }
 
   public Canvas editShape(String projectID, int layerIndex, int shapeIndex, Shape shape) {
     if (!projects.containsKey(projectID)) {
       throw new IllegalArgumentException("Project ID " + projectID + " does not exist!");
     }
+    Canvas canvas = projects.get(projectID);
 
     projectServiceLogger.info("editShape - " + shape.getHTML());
 
-    projects.get(projectID).getLayers().get(layerIndex).getShapes()
+    canvas.getLayers().get(layerIndex).getShapes()
         .set(shapeIndex, shape);
 
-    notifyObservers();
-    return projects.get(projectID);
+    return canvas;
   }
 
   public Canvas transformShape(String projectID) {
     if (!projects.containsKey(projectID)) {
       throw new IllegalArgumentException("Project ID " + projectID + " does not exist!");
     }
+    Canvas canvas = projects.get(projectID);
 
     // TODO: Transform Shape
 
-    return projects.get(projectID);
+    return canvas;
   }
 
   public Canvas deleteLayer(String projectID, int layerIndex) {
     if (!projects.containsKey(projectID)) {
       throw new IllegalArgumentException("Project ID " + projectID + " does not exist!");
     }
+    Canvas canvas = projects.get(projectID);
 
     projectServiceLogger.info("deleteLayer - Layer Index: " + layerIndex);
 
-    projects.get(projectID).getLayers().remove(layerIndex);
+    canvas.getLayers().remove(layerIndex);
 
-    notifyObservers();
-    return projects.get(projectID);
+    return canvas;
   }
 
   public Canvas deleteShape(String projectID, int layerIndex, int shapeIndex) {
     if (!projects.containsKey(projectID)) {
       throw new IllegalArgumentException("Project ID " + projectID + " does not exist!");
     }
+    Canvas canvas = projects.get(projectID);
 
     projectServiceLogger.info("deleteShape - Layer Index: " + layerIndex);
     projectServiceLogger.info("            - Shape Index: " + shapeIndex);
 
-    projects.get(projectID).getLayers().get(layerIndex).getShapes().remove(shapeIndex);
+    canvas.getLayers().get(layerIndex).getShapes().remove(shapeIndex);
 
-    return projects.get(projectID);
+    return canvas;
   }
 
   public ResponseEntity<Object> download(String projectID, String type)
@@ -188,6 +178,7 @@ public class ProjectService implements Subject {
     if (!projects.containsKey(projectID)) {
       throw new IllegalArgumentException("Project ID " + projectID + " does not exist!");
     }
+    Canvas canvas = projects.get(projectID);
 
     projectServiceLogger.info("Download in format: " + type);
 
@@ -211,7 +202,7 @@ public class ProjectService implements Subject {
         throw new IllegalArgumentException("Unknown file type");
     }
 
-    URI fileURI = downloadStrategy.download(projects.get(projectID), projectID);
+    URI fileURI = downloadStrategy.download(canvas, projectID);
     File file = new File(fileURI);
     Path path = Paths.get(fileURI);
     ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(file.toPath()));
@@ -228,33 +219,13 @@ public class ProjectService implements Subject {
   }
 
 
-  /*public void testUpdate() {
+  public void testUpdate() {
     setChanged();
     notifyObservers(new Object());
-  }*/
+  }
 
   public Map<String, Canvas> getProjects() {
     return projects;
-  }
-
-  @Override
-  public void registerObserver(Observer o) {
-    observers.add(o);
-  }
-
-  @Override
-  public void removeObserver(Observer o) {
-    int i = observers.indexOf(o);
-    if (i >= 0) {
-      observers.remove(i);
-    }
-  }
-
-  @Override
-  public void notifyObservers() {
-    for (Observer o : observers) {
-      o.update(projects);
-    }
   }
 }
 
--- implementation/src/main/java/main/input/RESTHandler.java
+++ implementation/src/main/java/main/input/RESTHandler.java
@@ -4,7 +4,6 @@ import canvas.Canvas;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import messages.ErrorResponse;
 import messages.RequestAddLayer;
 import messages.RequestAddShape;
 import messages.RequestDeleteLayer;
@@ -12,7 +11,6 @@ import messages.RequestDeleteShape;
 import messages.RequestEditCanvas;
 import messages.RequestEditLayer;
 import messages.RequestEditShape;
-import messages.Response;
 import messages.ServerResponse;
 import org.apache.batik.transcoder.TranscoderException;
 import org.slf4j.Logger;
@@ -39,16 +37,16 @@ public class RESTHandler {
 
   @CrossOrigin()
   @RequestMapping(value = "/create", method = RequestMethod.GET)
-  public Response createProject() {
+  public ServerResponse createProject() {
     ServerResponse response = new ServerResponse();
     response.setProjectID(projectService.createID());
-    response.setCanvas(projectService.createCanvas(response.getProjectID()));
+    response.setCanvas(projectService.createCanvas());
     return response;
   }
 
   @CrossOrigin()
   @RequestMapping(value = "/addLayer/{projectID}", method = RequestMethod.POST)
-  public Response addLayer(@PathVariable String projectID,
+  public ServerResponse addLayer(@PathVariable String projectID,
       @RequestBody RequestAddLayer request) {
     ServerResponse response = new ServerResponse(projectID);
     response.setCanvas(projectService.addLayer(projectID));
@@ -57,97 +55,49 @@ public class RESTHandler {
 
   @CrossOrigin()
   @RequestMapping(value = "/addShape/{projectID}", method = RequestMethod.POST)
-  public Response addShape(@PathVariable String projectID,
+  public ServerResponse addShape(@PathVariable String projectID,
       @RequestBody RequestAddShape request) {
-
     ServerResponse response = new ServerResponse(projectID);
-
-    try {
-      response.setCanvas(
-          projectService.addShape(projectID, request.getLayerIndex(), request.getShapeClass()));
-    } catch (ClassNotFoundException e) {
-      return new ErrorResponse("Class " + request.getShapeClass() + " does not exist.",
-          "/addShape/" + projectID);
-    } catch (IllegalAccessException e) {
-      return new ErrorResponse("layerIndex " + request.getLayerIndex() + " does not exist.",
-          "/addShape/" + projectID);
-    } catch (InstantiationException e) {
-      return new ErrorResponse("Could not instantiate object of class  " + request.getShapeClass(),
-          "/addShape/" + projectID);
-    } catch (Exception e) {
-      return new ErrorResponse("Unknown error.",
-          "/addShape/" + projectID);
-    }
-
+    response.setCanvas(
+        projectService.addShape(projectID, request.getLayerIndex(), request.getShapeClass()));
     return response;
   }
 
   @CrossOrigin()
   @RequestMapping(value = "/editCanvas/{projectID}", method = RequestMethod.POST)
-  public Response editCanvas(@PathVariable String projectID,
+  public ServerResponse editCanvas(@PathVariable String projectID,
       @RequestBody RequestEditCanvas request) {
-
     ServerResponse response = new ServerResponse(projectID);
-
-    try {
-      response
-          .setCanvas(projectService.editCanvas(projectID, request.getWidth(), request.getHeight()));
-    } catch (IllegalArgumentException e) {
-      return new ErrorResponse("Height and Width must both be positive!",
-          "/editCanvas/" + projectID);
-    } catch (IndexOutOfBoundsException e) {
-      return new ErrorResponse("projectID does not exist!", "/editCanvas/" + projectID);
-    }
-
+    response
+        .setCanvas(projectService.editCanvas(projectID, request.getWidth(), request.getHeight()));
     return response;
   }
 
 
   @CrossOrigin()
   @RequestMapping(value = "/editLayer/{projectID}", method = RequestMethod.POST)
-  public Response editLayer(@PathVariable String projectID,
+  public ServerResponse editLayer(@PathVariable String projectID,
       @RequestBody RequestEditLayer request) {
-
-    if (request.getLayerIndex() > projectService.getProjects().get(projectID).getLayers().size()) {
-      return new ErrorResponse(
-          "layerIndex out of bounds! Was: " + request.getLayerIndex() + " - Maximum layer index: "
-              + (projectService.getProjects().get(projectID).getLayers().size() - 1),
-          "/editlayer/" + projectID);
-    }
-
     ServerResponse response = new ServerResponse(projectID);
-
-    try {
-      response.setCanvas(
-          projectService.editLayer(projectID, request.getLayerIndex(), request.isVisible()));
-    } catch (IndexOutOfBoundsException e) {
-      return new ErrorResponse("projectID does not exist!", "/editLayer/" + projectID);
-    }
-
+    response.setCanvas(
+        projectService.editLayer(projectID, request.getLayerIndex(), request.isVisible()));
     return response;
   }
 
   @CrossOrigin()
   @RequestMapping(value = "/editShape/{projectID}", method = RequestMethod.POST)
-  public Response editShape(@PathVariable String projectID,
+  public ServerResponse editShape(@PathVariable String projectID,
       @RequestBody RequestEditShape request) {
-
     ServerResponse response = new ServerResponse(projectID);
-
-    try {
-      response.setCanvas(projectService
-          .editShape(projectID, request.getLayerIndex(), request.getShapeIndex(),
-              request.getShape()));
-    } catch (IllegalArgumentException e) {
-      return new ErrorResponse("projectID does not exist!", "/editShape/" + projectID);
-    }
-
+    response.setCanvas(projectService
+        .editShape(projectID, request.getLayerIndex(), request.getShapeIndex(),
+            request.getShape()));
     return response;
   }
 
   @CrossOrigin()
   @RequestMapping(value = "/transformShape/{projectID}", method = RequestMethod.POST)
-  public Response transformShape(@PathVariable String projectID,
+  public ServerResponse transformShape(@PathVariable String projectID,
       @RequestBody String request) {
     ServerResponse response = new ServerResponse(projectID);
     response.setCanvas(projectService.transformShape(projectID));
@@ -156,7 +106,7 @@ public class RESTHandler {
 
   @CrossOrigin()
   @RequestMapping(value = "/deleteLayer/{projectID}", method = RequestMethod.POST)
-  public Response deleteLayer(@PathVariable String projectID,
+  public ServerResponse deleteLayer(@PathVariable String projectID,
       @RequestBody RequestDeleteLayer request) {
     ServerResponse response = new ServerResponse(projectID);
     response.setCanvas(projectService.deleteLayer(projectID, request.getLayerIndex()));
@@ -164,8 +114,8 @@ public class RESTHandler {
   }
 
   @CrossOrigin()
-  @RequestMapping(value = "/deleteShape/{projectID}", method = RequestMethod.POST)
-  public Response deleteShape(@PathVariable String projectID,
+  @RequestMapping(value = "/deleteShape/{projectID}")
+  public ServerResponse deleteShape(@PathVariable String projectID,
       @RequestBody RequestDeleteShape request) {
     ServerResponse response = new ServerResponse(projectID);
     response.setCanvas(
