72b1aa6f0abce008afd707d14a48dd5aa5cc02c1
Daniel Sigmund <a01504922@unet.univie.ac.at>
2018-11-18 20:47:27 +0100

    Backend: Added javadoc for shapes package

--- implementation/src/main/java/shapes/Circle.java
+++ implementation/src/main/java/shapes/Circle.java
@@ -2,37 +2,81 @@ package shapes;
 
 import facilitators.Coordinate;
 
+/**
+ * Represents a Circle on the canvas.
+ * This class extends {@link Shape} by a center {@link Coordinate} and a radius.
+ *
+ * @see Shape
+ * @see RegularPolygon
+ * @see Star
+ */
 public class Circle extends Shape {
 
   private Coordinate center;
   private double radius;
 
+  /**
+   * Creates a new Circle with default attributes.
+   */
   public Circle() {
     this.center = new Coordinate(50, 50);
     this.radius = 50;
   }
 
+  /**
+   * Creates a new Circle with the specified attributes and default {@link Shape} attributes.
+   *
+   * @param center The center {@link Coordinate} of the Circle.
+   * @param radius The radius of the Circle.
+   */
   public Circle(Coordinate center, double radius) {
     this.center = center;
     this.radius = radius;
   }
 
+  /**
+   * Returns the center {@link Coordinate} of the Circle.
+   *
+   * @return The center {@link Coordinate} of the Circle.
+   */
   public Coordinate getCenter() {
     return center;
   }
 
+  /**
+   * Sets the center of the Circle.
+   *
+   * @param center The new center {@link Coordinate} for the Circle.
+   */
   public void setCenter(Coordinate center) {
     this.center = center;
   }
 
+  /**
+   * Returns the radius of the Circle.
+   *
+   * @return The radius of the Circle.
+   */
   public double getRadius() {
     return radius;
   }
 
+  /**
+   * Sets the radius of the Circle.
+   *
+   * @param radius The new radius for the Circle.
+   */
   public void setRadius(double radius) {
     this.radius = radius;
   }
 
+  /**
+   * Returns a String representation of the Circle's attributes as chained HTML attributes.
+   *
+   * @return A String representation of the Circle's attributes as chained HTML attributes.
+   *
+   * @see Shape#getHTMLAttributes()
+   */
   @Override
   protected String getHTMLAttributes() {
     return super.getHTMLAttributes() +
@@ -41,6 +85,11 @@ public class Circle extends Shape {
         " r=\"" + radius + "\"";
   }
 
+  /**
+   * Returns a String representation of the Circle as an SVG object in HTML.
+   *
+   * @return A String representation of the Circle as an SVG object in HTML.
+   */
   @Override
   public String getHTML() {
     return "<circle " + getHTMLAttributes() + "></circle>";
--- implementation/src/main/java/shapes/Star.java
+++ implementation/src/main/java/shapes/Star.java
@@ -4,26 +4,59 @@ import facilitators.Coordinate;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a Star on the canvas.
+ * It extends {@link RegularPolygon} by an inner radius (making the inherited radius the outer radius).
+ * The inner and outer radius determine the distance of the inner and outer vertices to the center respectively.
+ * This implementation of Star is equilateral and non-intersecting, but not equiangular.
+ *
+ * @see RegularPolygon
+ * @see Circle
+ * @see Shape
+ */
 public class Star extends RegularPolygon {
 
   private double innerRadius;
 
+  /**
+   * Creates a new Star with default attributes.
+   */
   public Star() {
     innerRadius = 25;
   }
 
+  /**
+   * Creates a new Star with the specified attributes and default {@link RegularPolygon} attributes.
+   *
+   * @param innerRadius The radius of the inner vertices of the Star.
+   */
   public Star(double innerRadius) {
     this.innerRadius = innerRadius;
   }
 
+  /**
+   * Returns the radius of the inner vertices of the Star.
+   *
+   * @return The radius of the inner vertices of the Star.
+   */
   public double getInnerRadius() {
     return innerRadius;
   }
 
+  /**
+   * Sets the radius of the inner vertices of the Star.
+   *
+   * @param innerRadius The new radius of the inner vertices of the Star.
+   */
   public void setInnerRadius(double innerRadius) {
     this.innerRadius = innerRadius;
   }
 
+  /**
+   * Calculates the {@link Coordinate Coordinates} of the Star's vertices and returns them as a {@link List}.
+   *
+   * @return A {@link List} of the {@link Coordinate Coordinates} of the Star's vertices.
+   */
   @Override
   protected List<Coordinate> getCoordinates() {
     List<Coordinate> coordinates = new ArrayList<>();
