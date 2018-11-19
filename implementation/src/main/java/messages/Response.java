0b55fd289dbe69157a656a8cf3b061361b2593da
Alexander Ramharter <a01363460@unet.univie.ac.at>
2018-11-19 02:28:06 +0100

    Added some more Javadoc in messages before changing to the other computer.

--- implementation/src/main/java/messages/Response.java
+++ implementation/src/main/java/messages/Response.java
@@ -1,5 +1,11 @@
 package messages;
 
+/**
+ * This abstract class holds a projectID to specify to which project/client/user the response belongs.
+ * The class gets extended and defined more by {@link ServerResponse}.
+ *
+ * @see ServerResponse
+ */
 public abstract class Response {
 
   String projectID;
--- implementation/src/main/java/messages/ServerResponse.java
+++ implementation/src/main/java/messages/ServerResponse.java
@@ -2,32 +2,62 @@ package messages;
 
 import canvas.Canvas;
 
+/**
+ * This class represents the server response. It always sends the whole canvas to the client. The {@link ServerResponse} extends the {@link Response}.
+ * Every response also has a projectID to specify to whom the response belongs. The class gets called everytime before the Server responses to the client.
+ *
+ * @see Response
+ * @see main.input.RESTHandler
+ */
 public class ServerResponse extends Response {
 
   private Canvas canvas;
 
+  /**
+   * Default constructor.
+   */
   public ServerResponse() {
     projectID = null;
     canvas = null;
   }
 
+  /**
+   * The projectID gets assigned to the {@link ServerResponse}.
+   * @param projectID hash generated projectID by {@link facilitators.Hasher} in {@link String}
+   */
   public ServerResponse(String projectID) {
     this.projectID = projectID;
     canvas = null;
   }
 
+  /**
+   * Returns the projectID. Gets called in createProject method in {@link main.input.RESTHandler}.
+   * @return returns projectID in {@link String}
+   */
   public String getProjectID() {
     return projectID;
   }
 
+  /**
+   * Sets the ProjectID to a new ID.
+   * @param projectID new projectID in {@link String}
+   */
   public void setProjectID(String projectID) {
     this.projectID = projectID;
   }
 
+  /**
+   * Returns the canvas of the Response. Gets called whenever the JSON object is created to send it to the client.
+   * @return returns a {@link Canvas}
+   */
   public Canvas getCanvas() {
     return canvas;
   }
 
+  /**
+   * Changes the Canvas. Gets called whenever the canvas is updated (e.g.: layer edited/deleted, shape edited/deleted).
+   * @param canvas new {@link Canvas} with new layers/shapes/parameters
+   */
   public void setCanvas(Canvas canvas) {
     this.canvas = canvas;
   }
