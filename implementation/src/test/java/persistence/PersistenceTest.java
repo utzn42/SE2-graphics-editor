aa43adf4d4d531398263611a9dc30a4f81f9d714
Utz Nisslmüller <a01426012@unet.univie.ac.at>
2019-01-11 09:43:48 +0100

    Created and passed isEmpty() and loadedProjectsTest() tests in projectSerializerTest().

--- implementation/src/test/java/persistence/PersistenceTest.java
+++ implementation/src/test/java/persistence/PersistenceTest.java
@@ -1,25 +1,37 @@
 package persistence;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import facilitators.Hasher;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import project.LoadedProject;
+import project.Project;
+import project.ProjectProxy;
 
 class PersistenceTest {
 
   private static final Logger persistenceTestLogger = LoggerFactory
       .getLogger(PersistenceTest.class);
+  private static Map<String, Project> testProjectList = new HashMap<>();
 
+  /**
+   * Deletes files within projects directory in order to be able to work in a clean, standardized
+   * environment.
+   */
   @BeforeEach
   void cleanProjects() {
     File dir = new File(
-        "C:\\Users\\utzn\\Desktop\\g2018w_se2_0304\\projects");           // how do I access this generally? Why does it work in persistence ("projects")?!
+        "C:\\Users\\utzn\\Desktop\\g2018w_se2_0304\\projects");                           // how do I access this generally? Why does it work in persistence ("projects")?!
     try {                                                                                           // Might need new directory structure
       FileUtils.cleanDirectory(dir);
       persistenceTestLogger.info("Successfully cleaned project directory.");
@@ -30,7 +42,18 @@ class PersistenceTest {
 
   @Test
   void ProjectSerializerTest() {
-    ProjectSerializer.getProjects();
-    assertTrue(true);
+    testProjectList = ProjectSerializer.getProjects();
+    assertTrue(testProjectList.isEmpty());
+    persistenceTestLogger.info("isEmpty() test was passed! Starting LoadedProject test..." + '\n');
+
+    String loadedProjectHash = new Hasher(0).getHash();
+    Project testLoadedProject = new LoadedProject(loadedProjectHash);
+    testProjectList.put((loadedProjectHash), testLoadedProject);
+    ProjectSerializer.putProject(testLoadedProject);
+    assertEquals(testProjectList, ProjectSerializer.getProjects());
+    persistenceTestLogger.info("LoadedProject test passed! Starting ProjectProxy test..." + '\n');
+
+    Project testProjectProxy = new ProjectProxy(loadedProjectHash);
+
   }
 }
